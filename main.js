16ba1c02075dc37baf47ab060ac82d6fc1fd369b
Yasmien Gayao <145944404+yjcgayao@users.noreply.github.com>
2023-11-03 19:00:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// Existing code in main.js
-
 window.addEventListener('scroll', () => {
   document.querySelector('#Home h1').style.marginTop = `${window.scrollY * 1.5}px`;
   document.querySelector('#Mountain1').style.marginBottom = `${106 - window.scrollY}px`;
